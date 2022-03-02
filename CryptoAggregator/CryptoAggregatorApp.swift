4f4f78e1017a7d7e463a83010e9d87363b967810
Bongani Bendile <bonganibendile@gmail.com>
2022-03-02 14:56:46 +0200

    Initial Commit

---
+++ CryptoAggregator/CryptoAggregatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAggregatorApp.swift
+//  CryptoAggregator
+//
+//  Created by Bongani Bendile on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAggregatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
