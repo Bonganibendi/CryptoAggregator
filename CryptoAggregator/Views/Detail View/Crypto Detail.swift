a0321147649f1862fcc87a8e3b68ab2773633d19
Bongani Bendile <bonganibendile@gmail.com>
2022-07-13 17:23:42 +0200

    Finalising DetailView Ui/Ux design.

--- CryptoAggregator/Views/Detail View/Crypto Detail.swift
+++ CryptoAggregator/Views/Detail View/Crypto Detail.swift
@@ -14,8 +14,7 @@ struct CryptoDetail: View {
     var body: some View {
         
         ScrollView {
-// MARK: - Chart Section
-
+            // MARK: - Chart Section
             VStack{
                 HStack {
                     LineView(data: [ instrument.sparkLine7D!.price!.first!,
@@ -50,172 +49,219 @@ struct CryptoDetail: View {
                                    ], title:"\(instrument.name!) Chart", legend: "Weekly Chart 6hr Intervals")
                 }.padding()
             }.frame(height: 300)
+                .navigationBarTitle("Detail")
             
             Divider()
-// MARK: - Coin Information Section
+            // MARK: - Coin Information Section
             VStack {
-                Text("Coin Information")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                
-                Divider()
-                VStack {
+                HStack{
                     Text("Coin Information")
                         .font(.title2)
                         .fontWeight(.bold)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding()
                     
-                    Divider()
-                    
-                    VStack {
-                        // MARK: Row 1 Coin Info.
-                        HStack(){
-                            VStack(alignment:.leading) {
-                                Text("Name")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                Text(instrument.name!)
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            VStack(alignment:.leading) {
-                                Text("Symbol")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                Text(instrument.symbol!)
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                        }.padding()
-                        // MARK: Row 2 Coin Info.
-                        HStack(){
-                            VStack(alignment:.leading) {
-                                Text("Market Cap")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                Text("\(instrument.marketCap ?? 0, specifier: "%.1f")")
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            VStack(alignment:.leading) {
-                                Text("Current Price")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                Text("\(instrument.currentPrice ?? 0, specifier: "%.2f")")
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                        }.padding()
-                        
-                        Text("Price Information")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding()
-                        
-                        Divider()
-                        // MARK: Row 1 Price Info.
-                        HStack(){
-                            VStack(alignment:.leading) {
-                                Text("Daily High")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                Text("\(instrument.dailyHigh ?? 0, specifier: "%.2f")")
-                                    .foregroundColor(.green)
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            VStack(alignment:.leading) {
-                                Text("Daily Low")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                Text("\(instrument.dailyLow ?? 0, specifier: "%.2f")")
-                                    .foregroundColor(.red)
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                        }.padding()
-                        // MARK: Row 2 Price Info.
-                        HStack(){
-                            VStack(alignment:.leading) {
-                                Text("Price Change 1h")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                if instrument.hourlyChange! > 0 {
-                                    Text("\(instrument.hourlyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.green)
-                                }else {
-                                    Text("\(instrument.hourlyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.red)
-                                }
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            VStack(alignment:.leading) {
-                                Text("Price Change 1d")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                if instrument.dailyChange! > 0 {
-                                    Text("\(instrument.dailyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.green)
-                                } else {
-                                    Text("\(instrument.dailyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.red)
-                                }
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                        }.padding()
-                        // MARK: Row 3 Price Info.
-                        HStack(){
-                            VStack(alignment:.leading) {
-                                Text("Price Change 7d")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                if instrument.weeklyChange! > 0 {
-                                    Text("\(instrument.weeklyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.green)
-                                } else {
-                                    Text("\(instrument.weeklyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.red)
-                                }
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            VStack(alignment:.leading) {
-                                Text("Price Change 14d")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                if instrument.biweeklyChange! > 0 {
-                                    Text("\(instrument.biweeklyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.green)
-                                } else {
-                                    Text("\(instrument.biweeklyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.red)
-                                }
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                        }.padding()
-                        // MARK: Row 4 Price Info.
-                        HStack(){
-                            VStack(alignment:.leading) {
-                                Text("Price Change 30d")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                if instrument.monthlyChange! > 0 {
-                                    Text("\(instrument.monthlyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.green)
-                                } else {
-                                    Text("\(instrument.monthlyChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.red)
-                                }
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            VStack(alignment:.leading) {
-                                Text("Price Change 1y")
-                                    .font(.headline)
-                                    .padding(.bottom, 1)
-                                if instrument.annualChange! > 0 {
-                                    Text("\(instrument.annualChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.green)
-                                } else {
-                                    Text("\(instrument.annualChange ?? 0, specifier: "%.2f")%")
-                                        .foregroundColor(.red)
-                                }
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                        }.padding()
-                    }
+                    urlImageDetail(urlString: instrument.image!, data: nil)
+                        .frame(width: 30, height: 30)
+                        .cornerRadius(10)
+                        .offset(x: -200)
                 }
-            }.navigationBarTitle("Detail")
+                
+                Divider()
+                VStack {
+                    // MARK: Row 1 Coin Info.
+                    HStack(){
+                        VStack(alignment:.leading) {
+                            Text("Name")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            Text(instrument.name!)
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        VStack(alignment:.leading) {
+                            Text("Symbol")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            Text(instrument.symbol!)
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                    }.padding()
+                    // MARK: Row 2 Coin Info.
+                    HStack(){
+                        VStack(alignment:.leading) {
+                            Text("Market Cap")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            Text("\(instrument.marketCap ?? 0, specifier: "%.1f")")
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        VStack(alignment:.leading) {
+                            Text("Current Price")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            Text("\(instrument.currentPrice ?? 0, specifier: "%.2f")")
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                    }.padding()
+                    
+                    Text("Price Information")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                    
+                    Divider()
+                    // MARK: Row 1 Price Info.
+                    HStack(){
+                        VStack(alignment:.leading) {
+                            Text("Daily High")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            Text("\(instrument.dailyHigh ?? 0, specifier: "%.2f")")
+                                .foregroundColor(.green)
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        VStack(alignment:.leading) {
+                            Text("Daily Low")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            Text("\(instrument.dailyLow ?? 0, specifier: "%.2f")")
+                                .foregroundColor(.red)
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                    }.padding()
+                    // MARK: Row 2 Price Info.
+                    HStack(){
+                        VStack(alignment:.leading) {
+                            Text("Price Change 1h")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            if instrument.hourlyChange! > 0 {
+                                Text("\(instrument.hourlyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.green)
+                            }else {
+                                Text("\(instrument.hourlyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.red)
+                            }
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        VStack(alignment:.leading) {
+                            Text("Price Change 1d")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            if instrument.dailyChange! > 0 {
+                                Text("\(instrument.dailyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("\(instrument.dailyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.red)
+                            }
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                    }.padding()
+                    // MARK: Row 3 Price Info.
+                    HStack(){
+                        VStack(alignment:.leading) {
+                            Text("Price Change 7d")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            if instrument.weeklyChange! > 0 {
+                                Text("\(instrument.weeklyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("\(instrument.weeklyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.red)
+                            }
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        VStack(alignment:.leading) {
+                            Text("Price Change 14d")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            if instrument.biweeklyChange! > 0 {
+                                Text("\(instrument.biweeklyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("\(instrument.biweeklyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.red)
+                            }
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                    }.padding()
+                    // MARK: Row 4 Price Info.
+                    HStack(){
+                        VStack(alignment:.leading) {
+                            Text("Price Change 30d")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            if instrument.monthlyChange! > 0 {
+                                Text("\(instrument.monthlyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("\(instrument.monthlyChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.red)
+                            }
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        VStack(alignment:.leading) {
+                            Text("Price Change 1y")
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .padding(.bottom, 1)
+                            if instrument.annualChange! > 0 {
+                                Text("\(instrument.annualChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("\(instrument.annualChange ?? 0, specifier: "%.2f")%")
+                                    .foregroundColor(.red)
+                            }
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                    }.padding()
+                }
+            }.offset(y: 70)
         }
+    }
+}
+
+//MARK: - Custom URL Image.
+struct urlImageDetail: View {
+    let urlString: String
+    
+    @State var data: Data?
+    
+    var body: some View {
+        if let data = data, let uiimage = UIImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 40, height: 40)
+                .cornerRadius(10)
+        }
+        else {
+            Image(systemName: "hourglass")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 40, height: 40)
+                .background(Color.gray)
+                .cornerRadius(10)
+                .onAppear {
+                    fetchData()
+                }
+        }
+    }
+    private func fetchData() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            self.data = data
+        }
+        task.resume()
     }
 }
